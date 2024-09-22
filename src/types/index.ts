f94e76454be43f20bf0d021aa06fd391973aeaff
coactis1 <carlos.actis@togetherdevs.com>
2024-09-22 19:57:23 +0200

    feat: Add podcast and episode types
    The code changes introduce new TypeScript type definitions for podcasts and episodes. This will improve type safety and enable better code organization and documentation.

---
+++ src/types/index.ts
@@ -0,0 +1,4 @@
+export type { Episode } from './episode';
+export type { Podcast } from './podcast';
+export type  { PodcastCard }  from './podcastCard';
+
---
+++ src/types/podcastCard.d.ts
@@ -0,0 +1,7 @@
+export interface PodcastCard {
+    id: string;
+    name: string;
+    artist: string;
+    imageUrl: string;
+    summary: string;
+  }
